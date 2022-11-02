842571ce5e278ba11282cf8cd6a6415ae534d2ab
Rodrigo Carrasco <rodrigo.carrasco40@inacapmail.cl>
2022-11-02 00:04:26 -0300

    agregados archivos estaticos para index

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require ('express')
 console.log(process.env)
 const app = express()
 
-app.get('/',(req,res)=>res.send("<h1>Hola Mundo</h1>"))
+//app.get('/',(req,res)=>res.send("<h1>Hola Mundo</>"))
+app.use(express.static('public'))//express usa los archivos estaticos
 
 app.listen(process.env.PORT||3000)
 
