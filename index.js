3eadb9b48e38724ba9b82aff4425af28eb4cf57c
Rodrigo Carrasco <rodrigo.carrasco40@inacapmail.cl>
2022-11-02 00:40:40 -0300

    agregado ejs profile

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require ('express')
+const path=require('path')
+const ejs=require('ejs')
 
-console.log(process.env)
 const app = express()
 
-//app.get('/',(req,res)=>res.send("<h1>Hola Mundo</>"))
-app.use(express.static('public'))//express usa los archivos estaticos
+//settings
+app.set('views',path.join(__dirname,'views'))//para dirigir a nodejs que aca estan los views
+app.set('view engine','ejs')
 
+//static files
+app.use(express.static(__dirname+'/public'))
+
+//routes
+app.get('/profile',(req,res)=>{
+    res.render('profile',{
+        name:'Rodrigo',
+        age:33
+    })
+})
+
+
+
+//start server 
 app.listen(process.env.PORT||3000)
 
 console.log('Listening port',process.env.PORT||3000)
